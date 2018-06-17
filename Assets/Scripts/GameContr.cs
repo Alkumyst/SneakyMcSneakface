fcd7ebd03b0424d67974894e26e42d4c687585da
KNOWN-UNIVERSE\jamhudsp <jamhudsp@uat.edu>
2018-06-17 12:42:26 -0700

    Added ability to exit game with escape button

--- Assets/Scripts/GameContr.cs
+++ Assets/Scripts/GameContr.cs
@@ -6,16 +6,21 @@ using UnityEngine.SceneManagement;
 public class GameContr : MonoBehaviour {
 
 	Keys otherComponent;
-
+	private GameContr gamemanager;
 
 	// Use this for initialization
 	void Start () {
 		otherComponent = GetComponent<Keys> ();
+
+		DontDestroyOnLoad (this);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
 	}
 
 
